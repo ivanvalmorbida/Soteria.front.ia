3ee1686aedb20d25f469fedae749c1e3a55610d1
Ivan Valmorbida <ivanvalmorbida@gmail.com>
2026-02-06 16:35:39 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://localhost:64934',
+        target: 'http://localhost:64934',
         changeOrigin: true,
         secure: false
       }
